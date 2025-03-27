f18cdf358b5dc0378581d5f328b414ca39a448f0
Andrzej Sulkowski <andrzej.sulkowski@protonmail.com>
2025-03-27 17:03:01 +0100

    feat-add-labeler

---
+++ contracts/Labeler.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract Labeler {
+    constructor() {}
+
+    function label() public pure returns (string memory) {
+        return "Labeler";
+    }
+}
